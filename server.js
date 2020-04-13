334cd4cf599b9d921806db5148da26045eb7a2f3
deathemporer <nitin.sh125@gmail.com>
2020-04-13 20:15:10 +0530

    update server.js

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ mongoose.connect('mongodb://localhost/URLShortener', {
     useNewUrlParser: true, useUnifiedTopology: true
 })
 app.set('view engine','ejs')
-//app.use(express.urlencoded({ extended:false}))
-app.use(express.static(__dirname + "/views"))
+app.use(express.urlencoded({ extended:false}))
 
 app.get('/', async (req,res) =>{
     const shortURLS = await ShortURL.find()
